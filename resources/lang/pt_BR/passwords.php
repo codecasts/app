9adb77d8cb4fef898f21ed5608375b0b003809d5
Diego Hernandes <iamhernandev@gmail.com>
2018-12-07 06:10:39 -0200

    partial translations

---
+++ resources/lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senhas devem conter pelo menos 6 caracteres e a confirmação deve ser igual a senha.',
+    'reset' => 'Sua senha foi restaurada!',
+    'sent' => 'Enviamos o link de recuperação de senha por email!',
+    'token' => 'Link de recuperação de senha inválido.',
+    'user' => "Não foi possível encontrar nenhum usuário para o email informado.",
+
+];
