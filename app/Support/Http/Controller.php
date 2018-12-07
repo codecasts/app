e76f03ec6c6fc67b1e6a9e914a0e075e9e745b82
Diego Hernandes <iamhernandev@gmail.com>
2018-12-06 22:07:48 -0200

    base controller

---
+++ app/Support/Http/Controller.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Codecasts\Support\Http;
+
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use League\Flysystem\Filesystem;
+use Codecasts\Domains\Users\User;
+
+/**
+ * Class Controller
+ *
+ * Base controller implementation.
+ */
+abstract class Controller extends BaseController
+{
+    // enabled traits.
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @var int Default page size.
+     */
+    protected $pageSize = 30;
+
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * @var Respond
+     */
+    protected $respond;
+
+    /**
+     * Controller constructor.
+     *
+     * Uses a middleware to always detect the running user on all requests.
+     */
+    public function __construct()
+    {
+        // assign a respond instance.
+        $this->respond = new Respond();
+
+        // user detection inline middleware.
+        $this->middleware(function ($request, $next) {
+            // assign user.
+            $this->user = auth()->user();
+            // complete request.
+            return $next($request);
+        });
+    }
+
+    /**
+     * Parse fractal includes.
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function parseIncludes(Request $request)
+    {
+        if ($request->filled('include')) {
+            return (array)explode(',', $request->get('include'));
+        }
+
+        return [];
+    }
+
+    /**
+     * Guard / Auth instance.
+     *
+     * @return Guard
+     */
+    protected function getAuth()
+    {
+        return auth()->guard();
+    }
+
+    /**
+     * Storage instance.
+     *
+     * @return Filesystem
+     */
+    protected function getStorage()
+    {
+        return app()->make('filesystem.cloud')->getDriver();
+    }
+
+    /**
+     * Request instance.
+     *
+     * @return \Illuminate\Http\Request
+     */
+    protected function getRequest()
+    {
+        return app()->make('request');
+    }
+}
